b6b9e0d39f12715247d57a0363f45d88f2635c74
Ayush Sharma <techtinkerzone9@gmail.com>
2025-09-15 17:33:02 +0530

    set up typescript

---
+++ backend/readme.md
@@ -0,0 +1,7 @@
+npm install express bcryptjs cookie-parser cors jsonwebtoken mongoose
+npm install -D typescript ts-node @types/node @types/express
+
+change type to module form commonjs
+
+genrate tsconfig.json file
+npx tsc --init  
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,35 @@
-import express from 'express'
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+const __dirname = path.resolve();
+
+// Load .env from the parent directory
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 
-const PORT = 3000
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+    origin: process.env.URL,
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+
+
+
+
+
+
 
 app.get('/', (req, res)=> {
     res.send('i am just checking')
